1cd7a309fec9816158ccbd23e2ddbcf24aa8b64b
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-12 00:20:32 +0530

    poll interface done

--- contracts/poll/BasePoll.sol
+++ contracts/poll/BasePoll.sol
@@ -1,39 +1,45 @@
 pragma solidity ^0.4.24;
 
-//import "../Protocol/IElectusProtocol.sol";
+import "electusprotocol/contracts/Protocol/IElectusProtocol.sol";
 import "./IPoll.sol";
 
 
 contract BasePoll /*is IPoll */{
     struct Proposal {
+        uint voteCount;
+        uint voteWeight;
         bytes32 name;
-        uint256 voteCount;
     }
 
     struct Voter {
         bool voted;
         uint8 vote;   // index of the voted proposal
         address delegate;
-        uint256 weight;
+        uint weight;
+        //uint timeStamp;
     }
 
-    string public name;
+    string public pollName;
+    string public pollType;
+    string public voterBaseLogic;
+
+    Proposal[] public proposals;
+    address[] public protocolAddresses;
 
     mapping(address => Voter) public voters;
 
-    Proposal[] public proposals;
-    //IElectusProtocol public protocol;
-
-    modifier isCurrentMember() {
-        //require(protocol.isCurrentMember(msg.sender), "Not an electus member");
+    modifier isValidVoter() {
+        require(canVote(msg.sender), "Not a valid voter");
         _;
     }
 
-    constructor(address _electusProtocol, bytes32[] _proposalNames) public {
-        //protocol = IElectusProtocol(_electusProtocol);
-        //return "address(1).isCurrentMember(msg.sender) | address(2).isCurrentMember(msg.sender) & address(3).isCurrentMember(msg.sender)";
+    constructor(address[] _protocolAddresses, bytes32[] _proposalNames) public {
+        protocolAddresses = _protocolAddresses;
+        voterBaseLogic = ""; //initialize here
+        pollName = ""; //initialize here
+        voterBaseLogic=""; //initialize here
         for (uint8 i = 0; i < _proposalNames.length; i++) {
-            proposals.push(Proposal({name: _proposalNames[i], voteCount: 0}));
+            proposals.push(Proposal({name: _proposalNames[i], voteCount: 0, voteWeight: 0}));
         }
     }
 
@@ -41,25 +47,60 @@ contract BasePoll /*is IPoll */{
         return name;
     }
 
+    function getPollType() external view returns (string) {
+        return pollType;
+    }
+
+    function getVoterBaseLogic() external view returns (string) {
+        return voterBaseLogic;
+    }
+
+    function getProtocolAddresses() external view returns (address[]) {
+        return protocolAddresses;
+    }
+
     function getProposals() external view returns (bytes32[]) {
-        bytes32[] proposalNames;
+        uint proposalCount = proposals.length;
+        bytes32[] memory proposalNames = new bytes32[proposalCount];
         for(uint index = 0; index < proposals.length; index++) {
-            proposalNames.push(proposals[index].name);
+            proposalNames[index] = proposals[index].name;
         }
         return proposalNames;
     }
 
     function canVote(address _to) external view returns (bool) {
-        return true;
+        //This is to be filled by user before deploying poll. Can't be modified after poll is deployed
+        return IElectusProtocol(protocolAddresses[0]).isCurrentMember(_to) && IElectusProtocol(protocolAddresses[1]).isCurrentMember(_to);
     }
 
-    function getVoteTally(uint _proposalId) external returns (uint[]) {
-        
+    function getVoteTally(uint _proposalId) external view returns (uint) {
+        return proposals[_proposalId].voteWeight;
     }
-    // function getVoterCount(uint _proposalId) external returns (uint);
-    // function getVoterBaseLogic() external returns (bytes32);
-    // function calculateVoteWeight(address _to) external returns (uint);
-    // function vote(uint _proposalId) external;
-    // function revokeVote() external;
-    // function onPollFinish(uint _winningProposal) external;
+
+    function getVoteTallies() external view returns (uint[]) {
+        uint proposalCount = proposals.length;
+        uint[] memory proposalWeights = new bytes32[proposalCount];
+        for(uint index = 0; index < proposals.length; index++) {
+            proposalWeights[index] = proposals[index].voteWeight;
+        }
+        return proposalWeights;
+    }
+
+    function getVoterCount(uint _proposalId) external view returns (uint) {
+        return proposals[_proposalId].voteCount;
+    }
+
+    function getVoterCounts() external view returns (uint[]) {
+        uint proposalCount = proposals.length;
+        uint[] memory proposalCounts = new bytes32[proposalCount];
+        for(uint index = 0; index < proposals.length; index++) {
+            proposalCounts[index] = proposals[index].voteCount;
+        }
+        return proposalCounts;
+    }
+
+    function calculateVoteWeight(address _to) external returns (uint);
+    function vote(uint _proposalId) external;
+    function revokeVote() external;
+    function onPollFinish(uint _winningProposal) external;
 }
--- contracts/poll/IPoll.sol
+++ contracts/poll/IPoll.sol
@@ -2,14 +2,34 @@ pragma solidity ^0.4.24;
 
 //Voterbase logic is hard coded and kept in the checkIfVoter function. The function takes input voterAddress and applies the necessary logic(custom implement) and returns a bool
 interface IPoll {
+    event TriedToVote(address indexed _from, uint indexed _to, uint voteWeight);
+    event CastVote(address indexed _from, uint indexed _to, uint voteWeight);
+
+    event RevokedVote(address indexed _from, uint indexed _to, uint voteWeight);
+
+    //gets the name of the poll e.g.: "Admin Election for Autumn 2018"
     function getName() external view returns (bytes32);
+    //gets Poll Type : Token (WAN) weighted poll
     function getPollType() external view returns (bytes32);
+    //gets voterbaselogic : "WanChain | US & China | Developers". Use this and protocolAddresses to fill in hover over.
     function getVoterBaseLogic() external view returns (bytes32);
+    //returns the protocol addresses e.g.: address for Wanchain, Us & China etc.
+    function getProtocolAddresses() external view returns (address[]);
+    //returns the proposal names
     function getProposals() external view returns (bytes32[]);
+    //returns whether the user can vote
     function canVote(address _to) external view returns (bool);
+    //gets the vote weight against the proposalid
     function getVoteTally(uint _proposalId) external view returns (uint);
+    //gets the vote weight against all proposals
+    function getVoteTallies() external view returns (uint[]);
+    //gets the vote count against the proposalid
     function getVoterCount(uint _proposalId) external view returns (uint);
+    //gets the vote count against all proposals
+    function getVoterCounts() external view returns (uint[]);
+
     function calculateVoteWeight(address _to) external view returns (uint);
+    //don't throw at all.. change state if canVote() .. else log
     function vote(uint _proposalId) external;
     function revokeVote() external;
     function onPollFinish(uint _winningProposal) external;
