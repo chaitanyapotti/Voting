b570c4e875e9101f8a1e2622b13492f0407d5eee
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-13 19:18:14 +0530

    added test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,76 +3,38 @@ var OnePersonOneVoteTest = artifacts.require(
 );
 var electusProtocol = artifacts.require("../contracts/protocol/protocol.sol");
 module.exports = function(deployer, network, accounts) {
-  // let instance1;
-  // let instance2;
-  // let instance3;
-  // deployer
-  //   .deploy(electusProtocol, "Wanchain", "WAN", { gas: 3000000 })
-  //   .then(protocol => {
-  //     instance1 = protocol.address;
-  //     protocol.assignTo(accounts[1], [0], {
-  //       from: accounts[0]
-  //     });
-  //     console.log(instance1, "ins1");
-  //   });
-  // deployer.deploy(electusProtocol, "US & China", "UC").then(protocol1 => {
-  //   instance2 = protocol1.address;
-  //   protocol1.assignTo(accounts[2], [0], {
-  //     from: accounts[0]
-  //   });
-  //   console.log(instance2, "ins2");
-  //   console.log(instance1, "ins1");
-  // });
-  // deployer.deploy(electusProtocol, "Developers", "DEV").then(protocol2 => {
-  //   instance3 = protocol2.address;
-  //   protocol2.assignTo(accounts[3], [0], {
-  //     from: accounts[0]
-  //   });
-  //   protocol2.assignTo(accounts[1], [0], {
-  //     from: accounts[0]
-  //   });
-  //   protocol2.assignTo(accounts[2], [0], {
-  //     from: accounts[0]
-  //   });
-  //   console.log(instance3, "ins3");
-  //   console.log(instance2, "ins2");
-  //   console.log(instance1, "ins1");
-  // });
-  // deployer.deploy(
-  //   OnePersonOneVoteTest,
-  //   [instance1, instance2, instance3],
-  //   ["0x68656c6c6f", "0x776f726c64"],
-  //   "0x57616e636861696e",
-  //   "0x41646d696e20456c656374696f6e20466f722032303138",
-  //   "0x4f6e6520506572736f6e204f6e6520566f7465"
-  // );
-  // .then(protocol2 => {
-  //   instance3 = protocol2.address;
-  //   protocol2.assignTo(accounts[3], [0], {
-  //     from: accounts[0]
-  //   })
-  //   protocol2.assignTo(accounts[1], [0], {
-  //     from: accounts[0]
-  //   })
-  //   protocol2.assignTo(accounts[2], [0], {
-  //     from: accounts[0]
-  //   })
-  // console.log(instance3, "ins3")
-  // console.log(instance2, "ins2")
-  // console.log(instance1, "ins1")
-  // })
-  // .then(deployer.deploy(electusProtocol, "chain", "WAN").then(protocol1 => {
-  //   instance2 = protocol1.address;
-  //   protocol1.assignTo(accounts[2], [0], {
-  //     from: accounts[0]
-  //   })
-  // console.log(instance2, "ins")
-  // console.log(instance1, "ins")
-  // }))
-  // .then(deployer.deploy(electusProtocol, "chain", "WAN").then(protocol2 => {
-  //   instance3 = protocol2.address;
-  // })).then(deployer.deploy(onePersonOneVote, [instance1, instance2]));
-  // deployer.link(electusProtocol, electusProtocolMetaData);
-  // deployer.deploy(electusProtocolMetaData);
-  //deployer.deploy(onePersonOneVote, [], []);
+  let instance1;
+  let instance2;
+  let instance3;
+  deployer
+    .then(function() {
+      return electusProtocol.new("Wanchain", "WAN", { gas: 3000000 });
+    })
+    .then(function(instance) {
+      instance1 = instance;
+      return electusProtocol.new("US & China", "UC", { gas: 3000000 });
+    })
+    .then(function(instanceB) {
+      instance2 = instanceB;
+      return electusProtocol.new("Developers", "DEV", { gas: 3000000 });
+    })
+    .then(function(instanceC) {
+      instance3 = instanceC;
+      return OnePersonOneVoteTest.new(
+        [instance1.address, instance2.address, instance3.address],
+        ["0x68656c6c6f", "0x776f726c64"],
+        "0x57616e636861696e",
+        "0x41646d696e20456c656374696f6e20466f722032303138",
+        "0x4f6e6520506572736f6e204f6e6520566f7465"
+      );
+    })
+    .then(function(instanceD) {
+      console.log(instance1.address);
+      console.log(instance2.address);
+      console.log(instance3.address);
+      instanceD.getPollType().then(function(returnValue) {
+        console.log(returnValue);
+        console.log(instanceD.address);
+      });
+    });
 };
--- test/onePersonOneVoteTest.js
+++ test/onePersonOneVoteTest.js
@@ -2,13 +2,39 @@ var OnePersonOneVoteTest = artifacts.require(
   "../contracts/testContracts/OnePersonOneVoteTest.sol"
 );
 
-contract("ElectusProtocol", function(accounts) {
-  // it("he is a current member", async () => {
-  //   const electusprotocol = await ElectusProtocol.deployed();
-  //   await electusprotocol.assignTo(accounts[1], [0], {
-  //     from: accounts[0]
-  //   });
-  //   data = await electusprotocol.isCurrentMember(accounts[1]);
-  //   assert.equal(data, true);
-  // });
+var electusProtocol = artifacts.require("../contracts/protocol/protocol.sol");
+
+contract("OnePersonOneVoteTest", function(accounts) {
+  beforeEach("setup", async () => {
+    protocol1Contract = await electusProtocol.new("Wanchain", "WAN", {
+      gas: 3000000
+    });
+    protocol2Contract = await electusProtocol.new("US & China", "UC", {
+      gas: 3000000
+    });
+    protocol3Contract = await electusProtocol.new("Developers", "DEV", {
+      gas: 3000000
+    });
+
+    pollContract = await OnePersonOneVoteTest.new(
+      [
+        protocol1Contract.address,
+        protocol2Contract.address,
+        protocol3Contract.address
+      ],
+      ["0x68656c6c6f", "0x776f726c64"],
+      "0x57616e636861696e",
+      "0x41646d696e20456c656374696f6e20466f722032303138",
+      "0x4f6e6520506572736f6e204f6e6520566f7465"
+    );
+  });
+  it("setup1", async () => {
+    console.log(protocol1Contract.address);
+    console.log(protocol2Contract.address);
+    console.log(protocol3Contract.address);
+    console.log(await pollContract.getProtocolAddresses());
+  });
+  it("setup2", async () => {
+    console.log(await pollContract.getProtocolAddresses());
+  });
 });
